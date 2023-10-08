6e6ab4416f317b96c2946228323f9279baaacf45
Anastasia Kim <123845505+kimavit@users.noreply.github.com>
2023-10-08 12:04:35 +0300

    Add files via upload

---
+++ count1.py
@@ -0,0 +1,14 @@
+#the sum of only positive numbers
+
+a = int (input())
+b = int (input())
+c = int (input())
+count = 0
+if a > 0:
+    count += a
+if b > 0:
+    count += b
+if c > 0:
+    count += c 
+print (count)
+
